da839f6ad1c724fd07a0a273a52776fe36149f03
Spiewak <bspiewak6@gmail.com>
2020-10-15 19:20:36 -0400

    started to figure out middleware

--- server.js
+++ server.js
@@ -5,11 +5,8 @@ const fs = require('fs');
 const PORT = process.env.PORT || 3006;
 const path = require('path');
 
+//  const { notes } = require('./')
 
-// initial get to test server
-app.get('/api/db/', (req, res) => {
-  res.send('TESTING 1, 2, 3');
-});
 
 // get route to send files to index.html
 app.get('/', (req, res) => {
@@ -21,8 +18,23 @@ app.get('/notes', (req, res) => {
   res.sendFile(path.join(__dirname, './public/notes.html'));
 });
 
-// initial port that is setup
+// create POST route to add new notes to JSON file
+app.post('/api/notes', (req, res) => {
+  // req.body is where our incoming content will be
+  console.log(req.body);
+  res.json(req.body);
+});
+
+// use express.static to link up the files that were provided by client
+app.use(express.static('public'));
+
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+
+// parse incoming JSON data
+app.use(express.json());
+
+// initial port that is setup to run the server
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
 });
-  
